1e75b3dca6fb8c15ee26ea0b7c75417e6b957635
aurel fontaine <ajfame11@gmail.com>
2020-06-12 21:43:59 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,7 +8,7 @@ class Show < ActiveRecord::Base
   
   def actors_list
     self.actors.collect do |actor|
-     "#{actor.full_name} - #{actor.show.name}"
+     "#{actor.full_name} - #{actor.shows.name}"
     end
   end
   
